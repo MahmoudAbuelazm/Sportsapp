7f7a34dabbc34ce5ebf8db88ff71d966907408bc
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2023-09-05 14:12:59 +0300

    edit for on boarding

--- sports_app/lib/widget/custom_button.dart
+++ sports_app/lib/widget/custom_button.dart
@@ -12,22 +12,25 @@ class CustomButton extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
         decoration: BoxDecoration(
-            boxShadow: [BoxShadow(blurRadius: 3)],
-             color: Colors.black.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(15.0)),
-        child: Row(mainAxisSize: MainAxisSize.min, children: [
-          Text(
-            "Skip",
-            style: GoogleFonts.allerta(fontSize: 16.0, color: Colors.white),
-          ),
-          SizedBox(
-            width: 15.0,
-          ),
-          Icon(
-            Icons.arrow_forward_sharp,
-            color: Colors.white,
-          )
-        ]),
+            border:
+                Border.all(color: Color.fromARGB(255, 42, 2, 95), width: 2.0),
+            borderRadius: BorderRadius.circular(50.0)),
+        child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Skip",
+                style: GoogleFonts.allerta(fontSize: 16.0, color: Colors.white),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Icon(
+                Icons.arrow_forward_sharp,
+                color: Colors.white,
+              )
+            ]),
       ),
     );
   }
